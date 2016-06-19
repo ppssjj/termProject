e24a91992ffe17ba1f6f4682b26c138e5eb46948
KwangEun An <KwangEun An@KwangEunAn_SAMSUNG.(none)>
2016-06-19 14:43:57 +0900

    코드만 정렬했셩

--- src/ComicBook.java
+++ src/ComicBook.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Vector;
 import java.awt.*;
 
@@ -18,7 +20,7 @@ class Images {
 	 */
 	public Images() { //
 		imageNames = new Vector<>();// 이미지 목록을 저장하기 위한 백터 컬렉션 객체를 인스턴스화.
-		imageFolder = "images";
+		imageFolder = "./images";
 		image_width = image_height = 100;
 	}
 
@@ -30,7 +32,7 @@ class Images {
 	 * @param height
 	 *            높이(y축)
 	 * @throws IllegalArgumentException
-	 *             높이 넓이가 음수인 경우, 에러를 표시
+	 *             높이 넓이가 음수인 경우, 에러 : 를 표시
 	 */
 	public Images setImageSize(int width, int height) {
 		if (width < 0 || height < 0)
@@ -46,11 +48,11 @@ class Images {
 	 * @param imageFolderName
 	 *            폴더 경로 변경
 	 */
-	public Images changeFolder(String imageFolderName) {
+	public Images changeFolder( String imageFolderName) {
 
-		// 만약 imageName이 빈 값이면 에러를 띄우고 프로그램 종료
-		if (imageFolderName == null && imageFolderName == "")
-			new IllegalArgumentException("! changeFolder : 폴더 이름이 공백이거나 null입니다.");
+		// 만약 imageName이 빈 값이면 에러 : 를 띄우고 프로그램 종료
+		if ( imageFolderName == null || imageFolderName.isEmpty())
+			throw new IllegalArgumentException("에러 :  changeFolder : 폴더 이름이 공백이거나 null입니다.");
 
 		System.out.print("changeFolder : 기존의 폴더 " + this.imageFolder + "에서 ");
 		this.imageFolder = imageFolderName;
@@ -71,17 +73,22 @@ class Images {
 	 * @param imageName
 	 *            추가할 이미지의 이름.
 	 * @throws IllegalArgumentException
-	 *             이미지 이름이 공백이거나 null인 경우 삭제 불가 에러를 띄운다.
+	 *             이미지 이름이 공백이거나 null인 경우 삭제 불가 에러 : 를 띄운다.
 	 */
 	public Images add(String imageName) {
 
-		// 만약 imageName이 빈 값이면 에러를 띄우고 프로그램 종료
-		if (imageName == null && imageName == "")
-			new IllegalArgumentException("! add : 이미지 이름이 공백이거나 null입니다.");
+		// 만약 imageName이 빈 값이면 에러 : 를 띄우고 프로그램 종료
+		if (imageName == null || imageName.isEmpty())
+			new IllegalArgumentException("에러 :  add : 이미지 이름이 공백이거나 null입니다.");
 
 		// 만약 등록하려는 이미지가 이미 등록된 이름과 동일한 경우 등록을 하지 않음.
 		if (imageNames.contains(imageName)) {
-			System.out.println("! add : 같은 이름을 가지는 이미지가 등록되어있습니다. 파일이 추가되지 않았습니다.");
+			System.out.println("에러 :  add : 같은 이름을 가지는 이미지가 등록되어있습니다. 이미지가 추가되지 않았습니다.");
+			return this;
+		}
+		
+		if(!(new File(getImagePath(imageName)).exists())){//여기가 오작동을 일으키는데.... 있는데 없다고 구라침 
+			System.out.println("경고 : add : 이미지 "+getImagePath(imageName)+"가 없습니다. 이미지가 추가되지 않았습니다.");
 			return this;
 		}
 
@@ -104,13 +111,13 @@ class Images {
 	 * @param imageName
 	 *            삭제할 이미지 이름
 	 * @throws IllegalArgumentException
-	 *             이미지 이름이 공백이거나 null인 경우 삭제 불가 에러를 띄운다.
+	 *             이미지 이름이 공백이거나 null인 경우 삭제 불가 에러 : 를 띄운다.
 	 */
 	public Images delete(String imageName) {
 
-		// 만약 imageName이 빈 값이면 에러를 띄우고 프로그램 종료
-		if (imageName == null && imageName == "")
-			new IllegalArgumentException("! del : 이미지 이름이 공백이거나 null입니다.");
+		// 만약 imageName이 빈 값이면 에러 : 를 띄우고 프로그램 종료
+		if (imageName == null || imageName.isEmpty())
+			throw new IllegalArgumentException("에러 :  del : 이미지 이름이 공백이거나 null입니다.");
 
 		// 이미지가 있는지 검색, 만약 발견하는 경우 해당 이미지 제거
 		for (int i = 0; i < imageNames.size(); i++) {
@@ -119,54 +126,97 @@ class Images {
 				System.out.println("del : " + imageName + "이 제거되었습니다.");
 				break;
 			}
-			System.out.println("! del : 삭제하려는 \'" + imageName + "\'이 없습니다. 삭제를 하지 못하였습니다.");
+			System.out.println("에러 :  del : 삭제하려는 \'" + imageName + "\'이 없습니다. 삭제를 하지 못하였습니다.");
 		}
 		return this;
 	}
 
 	/**
-	 * 이미지의 전체 경로를 반환해줌.
-	 * 중복되는 이름이 있는 경우 가장 먼저 등록된 이미지를 반환하므로 주의.
-	 * @param imageName 찾을 이미지의 이름 일부 
+	 * 이미지 크기를 조절, 이미지의 크기는 image_width, image_height의 두 변수에 영향을 받음.
+	 * 
+	 * @param srcImg
+	 *            조작할 이미지
 	 */
-	private String getImage(String imageName) {
+	private ImageIcon getScaledImage(ImageIcon imageIcon) {
+		BufferedImage resizedImg = new BufferedImage(image_width, image_height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = resizedImg.createGraphics();
+
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.drawImage(imageIcon.getImage(), 0, 0, image_width, image_height, null);
+		g2.dispose();
+
+		return new ImageIcon(resizedImg);
+	}
+
+	/**
+	 * 이미지의 전체 경로를 반환해줌.
+	 * 일부만 맞아도 가장 먼저 발견하는 이미지 이름을 반환.
+	 * <p>예제 <br />
+	 * abcd.jpg abc.jpg, bbb.jpg 순서대로 이미지를 등록한 경우 <br />
+	 * getImagepath(<b>"ab"</b>); 의 결과는 <br />
+	 * abcd.jpg를 돌려주고 끝남.
+	 * </p>
+	 * 
+	 * @param imageName
+	 *            찾을 이미지의 이름 일부
+	 */
+	private String getImagePath(String imageName) {
 		// 이미지가 있는지 검색, 만약 발견하는 경우 해당 이미지 제거
 		for (int i = 0; i < imageNames.size(); i++) {
 			if (imageNames.get(i).contains(imageName))
 				return imageFolder + "/" + imageNames.get(i);
 		}
-		System.out.println("! getImage : 찾으려는 \'" + imageName + "\'이 없습니다. 찾기를 실패하였습니다.");
+		System.out.println("에러 :  getImage : 찾으려는 \'" + imageName + "\'이 없습니다. 찾기를 실패하였습니다.");
 		return imageFolder + "/";
 	}
 
-	private String getImage(int imageNumber) {
+	/**
+	 * 이미지의 전체 경로를 반환해줌. 
+	 * 
+	 * @param imageNumber
+	 *            찾을 이미지 번호
+	 */
+	private String getImagePath(int imageNumber) {
 		// 이미지가 있는지 검색, 만약 발견하는 경우 해당 이미지 제거
 
-		if (!(imageNames.get(imageNumber)==null))
+		if (!(imageNames.get(imageNumber) == null))
 			return imageFolder + "/" + imageNames.get(imageNumber);
 
-		System.out.println("! getImage : 찾으려는 \'" + imageNumber + "\'가 없습니다. 찾기를 실패하였습니다.");
+		System.out.println("에러 :  getImage : 찾으려는 \'" + imageNumber + "\'가 없습니다. 찾기를 실패하였습니다.");
 		return imageFolder + "/";
 	}
 
-	public JLabel jLabel(String imageName) {
-		JLabel jl = new JLabel(new ImageIcon(getImage(imageName)));
-		jl.setSize(image_width, image_height);
-		return jl;
+	public JLabel getJLabel(String imageName) {
+		return new JLabel(getScaledImage(new ImageIcon(getImagePath(imageName))));
 	}
 
-	public JLabel jLabel(int imageNumber) {
-		JLabel jl = new JLabel(new ImageIcon(getImage(imageNumber)));
-		jl.setSize(image_width,image_height);
-		return jl;
+	public JLabel getJLabel(int imageNumber) {
+		return new JLabel(getScaledImage(new ImageIcon(getImagePath(imageNumber))));
 	}
-	
-	public ImageIcon imageIcon(int imageNumber){
-		return new ImageIcon(getImage(imageNumber));
+
+	/**
+	 * {@link add}함수를 사용하여 Vector에 등록된 이미지를 이미지 아이콘으로 반환하는 함수. 등록된 번호로 이미지를 찾음 
+	 * 
+	 * @param imageNumber
+	 *            찾을 이미지 번호
+	 * @return ImageIcon으로 반환
+	 */
+	public ImageIcon getImageIcon(int imageNumber) {
+		
+		if(imageNumber <0)
+			throw new IllegalArgumentException("에러 :  getImageIcon : 0보다 큰 수여야합니다.");
+		return new ImageIcon(getImagePath(imageNumber));
 	}
-	
-	public ImageIcon imageIcon(String imageName){
-		return new ImageIcon(getImage(imageName));
+
+	/**
+	 * {@link add}함수를 사용하여 Vector에 등록된 이미지를 이미지 아이콘으로 반환하는 함수. 이름으로 이미지를 찾음
+	 * 
+	 * @param imageName
+	 *            찾을 이미지 이름
+	 * @return ImageIcon으로 반환
+	 */
+	public ImageIcon getImageIcon(String imageName) {
+		return new ImageIcon(getImagePath(imageName));
 	}
 }
 
@@ -175,43 +225,30 @@ public class ComicBook extends JFrame {
 	String[] comicname;
 	Images image;
 	JLabel imgLabel;
-	// 이 코드는 위의 image로 대체됨
-	/*
-	 * ImageIcon[] images = { // �̹��� ��ü �迭 new
-	 * ImageIcon("images/.dragonball1.jpg"), new
-	 * ImageIcon("images/.ranma1.jpg"), new ImageIcon("images/.inuyasha1.jpg"),
-	 * new ImageIcon("images/.dgrayman1.jpg") }; JLabel imgLabel = new
-	 * JLabel(images[0]);// �̹��� ���̺� ������Ʈ ����
-	 */
 
 	ComicBook() {
-		comicname = new String[]{ "드래곤볼 1편", "란마1/2 1편", "이누야샤 1편", "디그레이멘 1화" };
-		image = new Images().add("dragonball1.jpg").add("ranma1.jpg").add("inuyasha1.jpg").add("dgrayman1.jpg");
+		comicname = new String[] { "드래곤볼 1편", "란마1/2 1편", "이누야샤 1편", "디그레이멘 1화" };
+		image = new Images().add("dragonball1.png").add("ranma1.jpg").add("inuyasha1.jpg").add("dgrayman1.jpg");
 
 		setTitle("만화책 대여 프로그램");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = getContentPane();
 		contentPane.setLayout(new FlowLayout());
 
-		JComboBox strCombo = new JComboBox(comicname);// �޺��ڽ� ����
-		// Action ������ ���
+		JComboBox strCombo = new JComboBox(comicname);
+		imgLabel = new JLabel();
 		strCombo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JComboBox cb = (JComboBox) e.getSource();// Action �̺�Ʈ�� �߻���
-															// �޺��ڽ� �˾Ƴ���
-
-				int index = cb.getSelectedIndex();// �޺��ڽ��� ���õ� ��������
-													// �ε��� ��ȣ �˾Ƴ���
-				System.out.println("what is index"+ index);
-				imgLabel.setIcon(image.imageIcon(index));// �ε����� �̹����� �̹��� ���̺�
-												// ������Ʈ�� ���
+				JComboBox cb = (JComboBox) e.getSource();
+				int index = cb.getSelectedIndex();
+				imgLabel.setIcon(image.getImageIcon(index));
 
 			}
 		});
 		contentPane.add(strCombo);
 		contentPane.add(imgLabel);
 
-		setSize(300, 250);
+		setSize(500, 500);
 		setVisible(true);
 	}
 
